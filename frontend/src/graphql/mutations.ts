aae7c8ac74965c1b94dce7c822a0ac1a2f127edd
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-11-12 23:05:26 +0200

    Feature: queries for the frontend

--- frontend/src/graphql/mutations.ts
+++ frontend/src/graphql/mutations.ts
@@ -1,15 +1,5 @@
 import { gql } from '@apollo/client'
 
-export const ALL_PERSONS = gql`
-  query {
-    allPersons  {
-      name
-      phone
-      id
-    }
-  }
-`
-
 export const CREATE_PERSON = gql`
   mutation createPerson($name: String!, $street: String!, $city: String!, $phone: String) {
     addPerson(
@@ -29,19 +19,6 @@ export const CREATE_PERSON = gql`
   }
 `
 
-export const FIND_PERSON = gql`
-  query findPersonByName($nameToSearch: String!) {
-    findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
-    }
-  }
-`
 
 export const EDIT_NUMBER = gql`
   mutation editNumber($name: String!, $phone: String!) {
