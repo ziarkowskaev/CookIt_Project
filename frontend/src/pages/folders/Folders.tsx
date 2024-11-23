3e73ef2351788daf3bfbc7c04ef23c953a2e8f77
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-11-23 11:57:58 +0200

    Setup for add folder.

---
+++ frontend/src/pages/folders/Folders.tsx
@@ -0,0 +1,14 @@
+import { Input } from "@/components/ui/input.tsx";
+import { Card, CardContent } from "../../components/ui/card.tsx";
+import { RecipesCarousel } from "./CarouselComp.tsx";
+
+const Folders = () => {
+    //Use List of Cards, but make cards the length of the page
+  return (
+    <div className="flex font-sans flex-col items-center">
+      
+    </div>
+  );
+};
+
+export default Folder;
