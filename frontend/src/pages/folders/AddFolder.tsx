b6bddbdb187ad01641415ab192919cfe4d06db0f
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-11-23 12:03:15 +0200

    Dialog for create folder

--- frontend/src/pages/folders/AddFolder.tsx
+++ frontend/src/pages/folders/AddFolder.tsx
@@ -16,39 +16,29 @@ const AddFolder = () => {
     return (
         <Dialog>
           <DialogTrigger asChild>
-            <Button variant="outline">Edit Profile</Button>
+            <Button variant="outline">Add Folder</Button>
           </DialogTrigger>
           <DialogContent className="sm:max-w-[425px]">
             <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
+              <DialogTitle>Add Folder</DialogTitle>
               <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
+              Add Folder
               </DialogDescription>
             </DialogHeader>
             <div className="grid gap-4 py-4">
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="name" className="text-right">
-                  Name
+                  Folder Title
                 </Label>
                 <Input
                   id="name"
-                  defaultValue="Pedro Duarte"
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Username
-                </Label>
-                <Input
-                  id="username"
-                  defaultValue="@peduarte"
+                  defaultValue="Folder 1"
                   className="col-span-3"
                 />
               </div>
             </div>
             <DialogFooter>
-              <Button type="submit">Save changes</Button>
+              <Button type="submit">Create</Button>
             </DialogFooter>
           </DialogContent>
         </Dialog>
