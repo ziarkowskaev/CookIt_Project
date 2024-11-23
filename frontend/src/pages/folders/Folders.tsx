2bf0debdccd8fdb94f8f06fa0aabe3e98261b8e1
Aaryan Nayar <aaryannayar@gmail.com>
2024-11-23 20:56:46 +0200

    Recipes shown based on categories; Navbar issue resolved for now; Recipes carousel on home page needs to be fixed

--- frontend/src/pages/folders/Folders.tsx
+++ frontend/src/pages/folders/Folders.tsx
@@ -3,12 +3,8 @@ import { Card, CardContent } from "../../components/ui/card.tsx";
 import { RecipesCarousel } from "./CarouselComp.tsx";
 
 const Folders = () => {
-    //Use List of Cards, but make cards the length of the page
-  return (
-    <div className="flex font-sans flex-col items-center">
-      
-    </div>
-  );
+  // Use List of Cards, but make cards the length of the page
+  return <div className="flex font-sans flex-col items-center"></div>;
 };
 
-export default Folder;
+export default Folders;
